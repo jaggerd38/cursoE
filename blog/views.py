f68c1fe0d5a18fcd01d3aa1abb12837f3a47c948
adderjagger <a.j.orozco038@hotmail.com>
2017-09-02 18:39:53 -0600

    commit para arreglar las vistas

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Publicacion
+# Create your views here.
+def listar_pub(request):
+    pub = Publicacion.objects.filter(fecha_publicacion__lte = timezone.now()).order_by('fecha_publicacion')
+
+    return render(request, 'blog/listar_pub.html', {'pub':pub})
